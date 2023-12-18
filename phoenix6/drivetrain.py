6a2ae0ffcd8e9d9ab4fa3d6237647bccc784ad5c
Scott <Scott@>
2023-12-18 10:38:36 -0500

    Updated with Phoenix6 beta5

--- phoenix6/drivetrain.py
+++ phoenix6/drivetrain.py
@@ -1,33 +1,55 @@
 import wpilib.drive
 from commands2 import Subsystem, Command
-import phoenix6
+from phoenix6.configs import TalonFXConfiguration, TalonFXConfigurator
+from phoenix6.hardware.talon_fx import TalonFX
 from phoenix6.controls.follower import Follower
-from phoenix6.signals.spn_enums import InvertedValue, NeutralModeValue, FeedbackSensorSourceValue, 
+from phoenix6.signals.spn_enums import (
+    InvertedValue,
+    NeutralModeValue,
+    FeedbackSensorSourceValue,
+)
+from phoenix6.sim import ChassisReference
+from phoenix6.controls import DutyCycleOut
 
 import constants
 import math
 from pyfrc.physics.units import units
 
+
 class DriveTrain(Subsystem):
     DT_TICKS_PER_MOTOR_REV = int(2048)
-    DT_TICKS_PER_INCH = (DT_TICKS_PER_MOTOR_REV * constants.DT_HIGH_GEAR_RATIO) / ((2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.inch))
+    DT_TICKS_PER_INCH = (DT_TICKS_PER_MOTOR_REV * constants.DT_HIGH_GEAR_RATIO) / (
+        (2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.inch)
+    )
 
     def __init__(self) -> None:
         super().__init__()
 
-        self._left_leader = phoenix6.TalonFX(constants.DT_LEFT_LEADER)
-        self._left_follower = phoenix6.TalonFX(constants.DT_LEFT_FOLLOWER)
-        self._right_leader = phoenix6.TalonFX(constants.DT_RIGHT_LEADER)
-        self._right_follower = phoenix6.TalonFX(constants.DT_LEFT_FOLLOWER)
+        self._left_leader = TalonFX(constants.DT_LEFT_LEADER)
+        self._left_follower = TalonFX(constants.DT_LEFT_FOLLOWER)
+        self._right_leader = TalonFX(constants.DT_RIGHT_LEADER)
+        self._right_follower = TalonFX(constants.DT_LEFT_FOLLOWER)
 
         self.__configure_left_side_drive()
         self.__configure_right_side_drive()
 
-    
+        self._left_duty_cyle: DutyCycleOut = DutyCycleOut(0, enable_foc=False)
+        self._left_duty_cyle.update_freq_hz = 0
+        self._right_duty_cycle: DutyCycleOut = DutyCycleOut(0, enable_foc=False)
+        self._right_duty_cycle.update_freq_hz = 0
+
+        self._left_follower.set_control(Follower(self._left_leader.device_id, False))
+        self._right_follower.set_control(Follower(self._right_leader.device_id, False))
+
+        self._left_leader.sim_state.Orientation = ChassisReference.Clockwise_Positive
+        self._left_follower.sim_state.Orientation = ChassisReference.Clockwise_Positive
+        self._right_leader.sim_state.Orientation = ChassisReference.Clockwise_Positive
+        self._right_follower.sim_state.Orientation = ChassisReference.Clockwise_Positive
+
     def __configure_left_side_drive(self) -> None:
         # Applying a new configuration will erase all other config settings since we start with a blank config
         # so each setting needs to be explicitly set here in the config method
-        config = phoenix6.TalonFXConfiguration()
+        config = TalonFXConfiguration()
 
         # Set the left side motors to be counter clockwise positive
         config.motor_output.inverted = InvertedValue.COUNTER_CLOCKWISE_POSITIVE
@@ -35,22 +57,15 @@ class DriveTrain(Subsystem):
         config.motor_output.neutral_mode = NeutralModeValue.BRAKE
         # PID controls will use integrated encoder
         config.feedback.feedback_sensor_source = FeedbackSensorSourceValue.ROTOR_SENSOR
-        
+
         # Apply the configuration to the motors
         self._left_leader.configurator.apply(config)
         self._left_follower.configurator.apply(config)
 
-        # Set the left follower to follow the leader
-        self._left_follower.set_control(Follower(constants.DT_LEFT_LEADER, False))
-
-        self._left_leader.sim_state.Orientation = ChassisReference.COUNTER_CLOCKWISE_POSITIVE
-        self._left_follower.sim_state.Orientation = ChassisReference.COUNTER_CLOCKWISE_POSITIVE
-
-
     def __configure_right_side_drive(self) -> None:
         # Applying a new configuration will erase all other config settings since we start with a blank config
         # so each setting needs to be explicitly set here in the config method
-        config = phoenix6.TalonFXConfiguration()
+        config = TalonFXConfiguration()
 
         # Set the left side motors to be counter clockwise positive
         config.motor_output.inverted = InvertedValue.CLOCKWISE_POSITIVE
@@ -58,13 +73,14 @@ class DriveTrain(Subsystem):
         config.motor_output.neutral_mode = NeutralModeValue.BRAKE
         # PID controls will use integrated encoder
         config.feedback.feedback_sensor_source = FeedbackSensorSourceValue.ROTOR_SENSOR
-        
+
         # Apply the configuration to the motors
-        self._left_leader.configurator.apply(config)
-        self._left_follower.configurator.apply(config)
+        self._right_leader.configurator.apply(config)
+        self._right_follower.configurator.apply(config)
 
-        # Set the left follower to follow the leader
-        self._left_follower.set_control(Follower(constants.DT_RIGHT_LEADER, False))
+    def drive_manually(self, forward: float, turn: float) -> None:
+        self._left_duty_cyle.output = forward + turn
+        self._right_duty_cycle.output = forward - turn
 
-        self._left_leader.sim_state.Orientation = ChassisReference.CLOCKWISE_POSITIVE
-        self._left_follower.sim_state.Orientation = ChassisReference.CLOCKWISE_POSITIVE
+        self._left_leader.set_control(self._left_duty_cyle)
+        self._right_leader.set_control(self._right_duty_cycle)
--- phoenix6/physics.py
+++ phoenix6/physics.py
@@ -6,6 +6,7 @@ from pyfrc.physics.units import units
 from wpimath.system.plant import DCMotor, LinearSystemId
 import math
 import typing
+import phoenix6
 
 import constants
 
@@ -19,9 +20,15 @@ class PhysicsEngine:
     one on each end of the track. Obviously, this is not particularly
     realistic, but it's good enough to illustrate the point
     """
-    DT_TICKS_PER_MOTOR_REV = int(2048) # Falcons are 2048
-    DT_TICKS_PER_INCH = (DT_TICKS_PER_MOTOR_REV * constants.DT_GEAR_RATIO) /  ((2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.inch))
-    DT_TICKS_PER_METER = (DT_TICKS_PER_MOTOR_REV * constants.DT_GEAR_RATIO) / ((2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.meter))
+
+    DT_TICKS_PER_MOTOR_REV = int(2048)  # Falcons are 2048
+    DT_TICKS_PER_INCH = (DT_TICKS_PER_MOTOR_REV * constants.DT_GEAR_RATIO) / (
+        (2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.inch)
+    )
+    DT_TICKS_PER_METER = (DT_TICKS_PER_MOTOR_REV * constants.DT_GEAR_RATIO) / (
+        (2 * math.pi) * constants.DT_WHEEL_DIAMETER.m_as(units.meter)
+    )
+
     def __init__(self, physics_controller: PhysicsInterface, robot: "MyRobot"):
         self.physics_controller = physics_controller
 
@@ -47,10 +54,18 @@ class PhysicsEngine:
             constants.DT_WHEEL_RADIUS_INCHES,
         )
         # fmt: on
-        self._l_lead_motor = robot._drivetrain._left_leader.sim_state
-        self._l_follow_motor = robot._drivetrain._left_follower.sim_state
-        self._r_lead_motor = robot._drivetrain._right_leader.sim_state
-        self._r_follow_motor = robot._drivetrain._right_follower.sim_state
+        self._l_lead_motor: phoenix6.sim.TalonFXSimState = (
+            robot._drivetrain._left_leader.sim_state
+        )
+        self._l_follow_motor: phoenix6.sim.TalonFXSimState = (
+            robot._drivetrain._left_follower.sim_state
+        )
+        self._r_lead_motor: phoenix6.sim.TalonFXSimState = (
+            robot._drivetrain._right_leader.sim_state
+        )
+        self._r_follow_motor: phoenix6.sim.TalonFXSimState = (
+            robot._drivetrain._right_follower.sim_state
+        )
 
     def update_sim(self, now: float, tm_diff: float) -> None:
         """
@@ -61,30 +76,57 @@ class PhysicsEngine:
                         time that this function was called
         """
 
-        # CTRE simulation is low-level, it ignores some of the things like motor 
+        # CTRE simulation is low-level, it ignores some of the things like motor
         # motor invresion etc.  WPILib wants +v to be forward.
         # Start the motor simulation work flow by passing robot battery voltage to sim motors
-        self._l_lead_motor.setBusVoltage(wpilib.RobotController.getBatteryVoltage())
-        self._l_follow_motor.setBusVoltage(wpilib.RobotController.getBatteryVoltage())
-        self._r_lead_motor.setBusVoltage(wpilib.RobotController.getBatteryVoltage())
-        self._r_follow_motor.setBusVoltage(wpilib.RobotController.getBatteryVoltage())
+        self._l_lead_motor.set_supply_voltage(
+            wpilib.RobotController.getBatteryVoltage()
+        )
+        self._r_lead_motor.set_supply_voltage(
+            wpilib.RobotController.getBatteryVoltage()
+        )
+        self._l_follow_motor.set_supply_voltage(
+            wpilib.RobotController.getBatteryVoltage()
+        )
+        self._r_follow_motor.set_supply_voltage(
+            wpilib.RobotController.getBatteryVoltage()
+        )
 
         # Apply the motor inputs to the simulation
-        self._drivesim.setInputs(self._l_lead_motor.getMotorOutputLeadVoltage(), self._r_lead_motor.getMotorOutputLeadVoltage())
+        self._drivesim.setInputs(
+            self._l_lead_motor.motor_voltage,
+            self._r_lead_motor.motor_voltage,
+        )
 
         # advance the simulation model a timing loop
         self._drivesim.update(tm_diff)
 
         # transform = self.drivetrain.calculate(speeds[self.LEFT_SPEED_INDEX], speeds[self.RIGHT_SPEED_INDEX], tm_diff)
         # pose = self.physics_controller.move_robot(transform)
-        self._l_lead_motor.set_raw_rotor_position( self.feet_to_encoder_ticks(self._drivesim.getLeftPositionFeet()) )
-        self._l_lead_motor.set_rotor_velocity( self.velocity_feet_to_talon_ticks(self._drivesim.getLeftVelocityFps()) )
-        self._r_lead_motor.set_raw_rotor_position( self.feet_to_encoder_ticks(self._drivesim.getRightPositionFeet()) )
-        self._r_lead_motor.set_rotor_velocity( self.velocity_feet_to_talon_ticks(self._drivesim.getRightVelocityFps()) )
-        self._l_follow_motor.set_raw_rotor_position( self.feet_to_encoder_ticks(self._drivesim.getLeftPositionFeet()) )
-        self._l_follow_motor.set_rotor_velocity( self.velocity_feet_to_talon_ticks(self._drivesim.getLeftVelocityFps()) )
-        self._r_follow_motor.set_raw_rotor_position( self.feet_to_encoder_ticks(self._drivesim.getRightPositionFeet()) )
-        self._r_follow_motor.set_rotor_velocity( self.velocity_feet_to_talon_ticks(self._drivesim.getRightVelocityFps()) )
+        self._l_lead_motor.set_raw_rotor_position(
+            self.__feet_to_encoder_ticks(self._drivesim.getLeftPositionFeet())
+        )
+        self._l_lead_motor.set_rotor_velocity(
+            self.__velocity_feet_to_talon_ticks(self._drivesim.getLeftVelocityFps())
+        )
+        self._r_lead_motor.set_raw_rotor_position(
+            self.__feet_to_encoder_ticks(self._drivesim.getRightPositionFeet())
+        )
+        self._r_lead_motor.set_rotor_velocity(
+            self.__velocity_feet_to_talon_ticks(self._drivesim.getRightVelocityFps())
+        )
+        self._l_follow_motor.set_raw_rotor_position(
+            self.__feet_to_encoder_ticks(self._drivesim.getLeftPositionFeet())
+        )
+        self._l_follow_motor.set_rotor_velocity(
+            self.__velocity_feet_to_talon_ticks(self._drivesim.getLeftVelocityFps())
+        )
+        self._r_follow_motor.set_raw_rotor_position(
+            self.__feet_to_encoder_ticks(self._drivesim.getRightPositionFeet())
+        )
+        self._r_follow_motor.set_rotor_velocity(
+            self.__velocity_feet_to_talon_ticks(self._drivesim.getRightVelocityFps())
+        )
 
         # Update the gyro simulation
         # -> FRC gyros are positive clockwise, but the returned pose is positive
@@ -94,25 +136,29 @@ class PhysicsEngine:
         # self.navx_yaw.set(-pose.rotation().degrees())
 
         self.physics_controller.field.setRobotPose(pose)
-        
 
-    def feet_to_encoder_ticks(self, distance_in_feet: float) -> int:
+    def __feet_to_encoder_ticks(self, distance_in_feet: float) -> int:
         return int((distance_in_feet * 12) * self.DT_TICKS_PER_INCH)
-    
 
-    def velocity_feet_to_talon_ticks(self, velocity_in_feet: float) -> int:
-        wheel_rotations_per_second = (velocity_in_feet * 12) / (2 * math.pi * constants.DT_WHEEL_DIAMETER.m_as(units.inch))
-        wheel_rotations_per_100ms = (wheel_rotations_per_second * constants.DT_GEAR_RATIO) / 10
+    def __velocity_feet_to_talon_ticks(self, velocity_in_feet: float) -> int:
+        wheel_rotations_per_second = (velocity_in_feet * 12) / (
+            2 * math.pi * constants.DT_WHEEL_DIAMETER.m_as(units.inch)
+        )
+        wheel_rotations_per_100ms = (
+            wheel_rotations_per_second * constants.DT_GEAR_RATIO
+        ) / 10
         motor_rotations_per_100ms = wheel_rotations_per_100ms * constants.DT_GEAR_RATIO
         return int(motor_rotations_per_100ms * self.DT_TICKS_PER_MOTOR_REV)
-    
 
-    def meters_to_encoder_ticks(self, distance_in_meters: float) -> int:
+    def __meters_to_encoder_ticks(self, distance_in_meters: float) -> int:
         return int((distance_in_meters * self.DT_TICKS_PER_METER))
-    
 
-    def velocity_meters_to_talon_ticks(self, velocity_in_meters: float) -> int:
-        wheel_rotations_per_second = velocity_in_meters / (2 * math.pi * constants.DT_WHEEL_DIAMETER.m_as(units.meter))
-        wheel_rotations_per_100ms = (wheel_rotations_per_second * constants.DT_GEAR_RATIO) / 10
+    def __velocity_meters_to_talon_ticks(self, velocity_in_meters: float) -> int:
+        wheel_rotations_per_second = velocity_in_meters / (
+            2 * math.pi * constants.DT_WHEEL_DIAMETER.m_as(units.meter)
+        )
+        wheel_rotations_per_100ms = (
+            wheel_rotations_per_second * constants.DT_GEAR_RATIO
+        ) / 10
         motor_rotations_per_100ms = wheel_rotations_per_100ms * constants.DT_GEAR_RATIO
         return int(motor_rotations_per_100ms * self.DT_TICKS_PER_MOTOR_REV)
